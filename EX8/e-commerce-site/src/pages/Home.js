77e2c2737d57b490850d69845a7e646cee23fa28
Karan T <karant17032005@gmail.com>
2024-11-12 10:21:39 +0530

    ex9

--- EX8/e-commerce-site/src/pages/Home.js
+++ EX8/e-commerce-site/src/pages/Home.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Home.css';
 
 const Home = () => {
   return (
-    <div className="container">
-      <h1>Welcome to Our E-commerce Store!</h1>
-      <p>
-        Discover a wide variety of products at unbeatable prices. Whether you’re looking for electronics, fashion, or home essentials, we’ve got it all! 
-      </p>
-      <p>
-        Browse through our collection to find the best deals and enjoy a seamless shopping experience.
-      </p>
-      <Link to="/products">
-        <button className="explore-button">Explore Products</button>
-      </Link>
+    <div>
+      <h1>Welcome to Our E-commerce Site</h1>
+      <p>Discover amazing products!</p>
     </div>
   );
 };
